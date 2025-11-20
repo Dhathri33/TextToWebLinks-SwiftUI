b232e2938975ecb36d4f159e341ff030019fe5e1
Dhathri33 <dbathini@luc.edu>
2025-11-20 11:11:40 -0600

    Initial Commit

---
+++ TextToWebLinks SwiftUI/TextToWebLinks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToWebLinks_SwiftUIApp.swift
+//  TextToWebLinks SwiftUI
+//
+//  Created by Dhathri Bathini on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TextToWebLinks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
