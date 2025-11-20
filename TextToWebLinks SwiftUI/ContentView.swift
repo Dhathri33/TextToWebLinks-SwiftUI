19e0cfbf323b705007acfb339ba1d4e5cda373e8
Dhathri33 <dbathini@luc.edu>
2025-11-20 11:22:14 -0600

    Designed the UI

--- TextToWebLinks SwiftUI/ContentView.swift
+++ TextToWebLinks SwiftUI/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HeaderView()
+            TextFieldView
+            Spacer()
         }
         .padding()
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Text("Text To Web")
+                .font(.title)
+                .bold()
+            Text("Convert text to clickable web links instantly")
+                .font(.subheadline)
+                .foregroundStyle(.gray)
+        }
+    }
+}
+
+extension ContentView {
+    var TextFieldView: some View {
+        TextField("Enter text here", text: $text)
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
     }
 }
 
